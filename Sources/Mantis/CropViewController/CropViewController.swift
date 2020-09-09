1fbada7d8486eb94afabee181f9555e8f6249639
Andrei Novikov <anovikov.brest@gmail.com>
2020-09-09 16:29:11 +0300

    Add set new image

--- Sources/Mantis/CropViewController/CropViewController.swift
+++ Sources/Mantis/CropViewController/CropViewController.swift
@@ -392,4 +392,8 @@ extension CropViewController {
         }
         cropView.viewModel.setBetweenOperationStatus()
     }
+    
+    public func updateImage(_ image: UIImage) {
+        cropView.image = image
+    }
 }
